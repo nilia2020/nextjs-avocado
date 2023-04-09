046ea8e00a93dfdbec45e5b1f49d9f11b0d32cc2
jorge niglia <jorge.niglia@gmail.com>
2023-04-09 11:01:02 -0300

    data length

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -4,7 +4,8 @@ import DataBase from '@database'
 const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
   const database = new DataBase()
   const allEntries = await database.getAll()
-  res.status(200).json({ data: allEntries })
+  const length = allEntries.length
+  res.status(200).json({ data: allEntries, length })
 }
 
 export default allAvos
