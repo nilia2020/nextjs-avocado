381b8fd3ace36c7d8b9e01ef4ce4c45947751e88
jorge niglia <jorge.niglia@gmail.com>
2023-04-16 17:43:39 -0300

    Server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react'
 import Layout from '@components/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+export const getServerSideProps = async () => {
+  const response = await fetch(
+    'https://nextjs-avocado-i0xw0ov53-nilia2020.vercel.app/api/avo'
+  )
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  return {
+    props: {
+      productList,
+    },
+  }
+}
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
-
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
