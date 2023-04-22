aad48ecef1b2ab675fabcdb620fb42d3730f8fc1
jorge niglia <jorge.niglia@gmail.com>
2023-04-22 18:19:46 -0300

    export static files

--- pages/users/[userId]/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const UserId = () => {
-  const router = useRouter();
-  const { userId } = router.query;
-  return <div>El usuario: {userId}</div>;
-};
-
-export default UserId;
